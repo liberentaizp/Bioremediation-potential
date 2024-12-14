08e993b0ebd4361dfe404d1bfe53568ed26eebb2
liberentaizp <167868264+liberentaizp@users.noreply.github.com>
2024-12-14 15:00:01 +0000

    Update prokka_annotation.sh

--- prokka_annotation.sh
+++ prokka_annotation.sh
@@ -2,4 +2,8 @@
 conda install -c conda - forge -c bioconda -c defaults prokka
 
 #Annotate all the FASTA files. Substitute the fasta folder path
-for file in fastafolderpath/*. fna ; do tag =${ file %. fna }; prokka -- prefix " $tag " -- locustag " $tag" -- genus Pseudomonas -- species putida -- strain " $tag " -- outdir ../Prokka_gff3 /20241113 _" $tag " _prokka -- force " $file "; done
+for file in fastafolderpath/*. fna
+do
+  tag =${ file %. fna }
+  prokka -- prefix " $tag " -- locustag " $tag" -- genus Pseudomonas -- species putida -- strain " $tag " -- outdir ../Prokka_gff3 /20241113 _" $tag " _prokka -- force " $file "
+done
